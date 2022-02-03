1850753f876d4992e8699a07c5b954af7ec9ff00
PiusKariuki <piuskariuki19@gmail.com>
2022-02-03 05:41:54 +0300

    first

---
+++ src/modules/Home/components/Searching.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Topbar from './Topbar';
+
+const Searching = () => {
+  return <div className="flex flex-col">
+     <Topbar />
+  </div>;
+};
+
+export default Searching;
