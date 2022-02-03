5248f2b9a0205079c109844f5fa4bf223e9c5407
PiusKariuki <piuskariuki19@gmail.com>
2022-02-03 11:27:26 +0300

    foxtrot

--- src/modules/Home/components/Products.tsx
+++ src/modules/Home/components/Products.tsx
@@ -15,23 +15,23 @@ const Products = () => {
 			<p className="text-black-40 text-[2.2rem] font-[600] mb-[2rem]">
 				Recommended products
 			</p>
-			{/* <div className="scroller flex flex-row gap-x-8 overflow-x-auto w-screen"> */}
-			{/* {products.map((product: any, key: number) => (
+			<div className="scroller flex flex-row gap-x-8 overflow-x-auto w-screen">
+			{products.map((product: any, key: number) => (
 				<ViewProduct
-					name={product.name}
-					userName={product.ownerId.userName}
-					img={product.images[0]}
-					price={product.price}
+					name={product?.name}
+					userName={product?.ownerId?.userName}
+					img={product?.images[0]}
+					price={product?.price}
 					key={key}
 				/>
-			))} */}
-			<ViewProduct
+			))}
+			{/* <ViewProduct
 				name={products[0]?.name}
 				userName={products[0]?.ownerId?.userName}
 				img={products[0]?.images[0]}
 				price={products[0]?.price}
-			/>
-			{/* </div> */}
+			/> */}
+			</div>
 		</div>
 	);
 };
