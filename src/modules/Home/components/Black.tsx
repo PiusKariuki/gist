14e83d0e163a60f71b94fe8bd530bb5221b19c12
PiusKariuki <piuskariuki19@gmail.com>
2022-03-04 15:59:19 +0300

    pushing

--- src/modules/Home/components/Black.tsx
+++ src/modules/Home/components/Black.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Black = () => {
 	return (
-		<div className="bg-[#000000CC] text-[#00bcd7] cursor-pointer flex space-x-8 py-[0.2rem] 
+		<div className="bg-[#000000CC] text-[#00bcd7] cursor-pointer flex space-x-4 py-[0.2rem] 
       md:py-[0.5rem] md:px-[4.5rem] items-center ">
 			<p className="text-[0.9rem] font-[600]">Features</p>
 			<p className="text-[0.9rem] font-[600]">Beauty</p>
