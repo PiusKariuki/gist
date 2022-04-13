01b0d7de0fcc297e2d76a3a04c36983c1d8637fc
Fredrick Mundia <66358227+freddysundowner@users.noreply.github.com>
2022-04-13 23:11:38 +0300

    live

--- src/modules/Home/components/Upcoming/Dates.tsx
+++ src/modules/Home/components/Upcoming/Dates.tsx
@@ -2,18 +2,28 @@ import moment from "moment";
 import React, { useEffect, useState } from "react";
 
 const Dates: React.FC<{ date: any }> = ({ date }) => {
-	const [formatted, setFormmated] = useState<any>("");
+  const [formatted, setFormmated] = useState<any>("");
+  const [formattedtime, setFormmatedTime] = useState<any>("");
 
-	useEffect(() => {
-		console.log(moment(date).calendar());
-      setFormmated(moment(date).endOf('day').fromNow());	
-	}, [date]);
+  useEffect(() => {
+    //  console.log(moment(date).calendar());
+    setFormmated(moment(date).calendar().split(" at")[0]+" at");
+    setFormmatedTime(moment(date).calendar().split(" at")[1]);
 
-	return (
-		<div className="absolute  w-full top-5 left-2 z-20">
-			<p className="text-base font-black text-blue-20">{date > 0 && formatted}</p>
-		</div>
-	);
+   //  console.log(moment(date).calendar().split(" at")[0]);
+   //  console.log(moment(date).calendar().split(" at")[1]);
+	 ;
+  }, [date]);
+
+  return (
+    <div className="absolute  w-full top-5 left-2 z-20">
+      <p className="text-base font-black text-blue-20">
+			  {date > 0 && formatted}
+			  <br/>
+        {date > 0 && formattedtime}
+      </p>
+    </div>
+  );
 };
 
 export default Dates;
