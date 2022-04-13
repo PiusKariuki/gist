a232f934f9295ddb9a4479f721a86cf689d76a53
PiusKariuki <piuskariuki19@gmail.com>
2022-04-13 23:30:00 +0300

    styling upcoming

--- src/modules/Home/components/Upcoming/Dates.tsx
+++ src/modules/Home/components/Upcoming/Dates.tsx
@@ -2,29 +2,30 @@ import moment from "moment";
 import React, { useEffect, useState } from "react";
 
 const Dates: React.FC<{ date: any }> = ({ date }) => {
-  const [formatted, setFormmated] = useState<any>("");
-  const [formattedtime, setFormmatedTime] = useState<any>("");
+	const [formatted, setFormmated] = useState<any>("");
+	const [formattedtime, setFormmatedTime] = useState<any>("");
 
 	useEffect(() => {
-	  console.log(moment(date).calendar().split(" at").length);
-	  
-    setFormmated(
-      moment(date).calendar().split(" at").length > 1
-        ? moment(date).calendar().split(" at")[0] + " at"
-        : moment(date).calendar()
-    );
-    setFormmatedTime(moment(date).calendar().split(" at")[1]);
-  }, [date]);
+		console.log(moment(date).calendar().split(" at").length);
 
-  return (
-    <div className="absolute  w-full top-5 left-2 z-20">
-      <p className="text-base font-black text-blue-20">
-        {date > 0 && formatted}
-        <br />
-        {date > 0 && formattedtime}
-      </p>
-    </div>
-  );
+		setFormmated(
+			moment(date).calendar().split(" at").length > 1
+				? moment(date).calendar().split(" at")[0] + " at"
+				: moment(date).calendar()
+		);
+		setFormmatedTime(moment(date).calendar().split(" at")[1]);
+	}, [date]);
+
+	return (
+		<div className="absolute  w-full top-5 left-2 z-20">
+			<p className=" font-black text-blue-20 ">
+				<span className="text-lg">{date > 0 && formatted}</span>
+				<br />
+            <span className="text-3xl">&nbsp;{date > 0 && formattedtime}</span>
+				
+			</p>
+		</div>
+	);
 };
 
 export default Dates;
