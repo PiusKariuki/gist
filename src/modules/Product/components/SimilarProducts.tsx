989942431cf4cfb5f9a4a6eb1aa98622c78b5cc2
PiusKariuki <piuskariuki19@gmail.com>
2022-04-13 11:40:45 +0300

    scroll top on products

--- src/modules/Product/components/SimilarProducts.tsx
+++ src/modules/Product/components/SimilarProducts.tsx
@@ -1,39 +1,62 @@
 import ShopProducts from "modules/shop/components/ShopProducts";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useSpinner from "shared/components/spinner/useSpinner";
 import useFetch from "shared/hooks/useFetch";
 
-const SimilarProducts: React.FC<{ shopId: string }> = ({ shopId }) => {
+const SimilarProducts: React.FC<{
+	shopId: string;
+	getProduct: (prodId: any) => void;
+	userName: string;
+}> = ({ shopId, getProduct, userName }) => {
 	const { data, getObject, load } = useFetch();
 	const [filteredProducts, setFilteredProducts] = useState<any>([]);
 	const { productId } = useParams();
 
+	let navigate = useNavigate();
+
+	const { renderSpinner } = useSpinner();
+
+	/**
+	 * Description: get the shop products
+	 * @param {any} (
+	 * @returns {any}
+	 */
 	useEffect(() => {
 		getObject(`/products/${shopId}`, "GET", {});
-	}, []);
+	}, [shopId, productId]);
 
+	/**
+	 * Description get filtered products once data is loaded
+	 * @param {any} (
+	 * @returns {any}
+	 */
 	useEffect(() => {
 		setFilteredProducts(
-			data.filter((product: any) => product._id !== productId)
+			data?.length && data?.filter((product: any) => product._id !== productId)
 		);
-	}, []);
-	return (
-		<div className="flex flex-col w-full mt-8">
-			<p
-				className="text-[2rem] md:text-[2.5rem] text-black-40 font-[700]
-                   border-b-4 ">
-				More products
-			</p>
+	}, [data]);
 
+	return (
+		<div className="flex flex-col w-full mt-16">
+			{filteredProducts?.length > 0 ? (
+				<p
+					className="text-lg md:text-2xl text-black-40 font-semibold
+            border-b-4">
+					More products from {userName}
+				</p>
+			) : null}
+			{renderSpinner(load)}
 			<div
 				className="flex flex-row flex-wrap w-full justify-items-start gap-y-4 gap-x-6
                mt-8">
-				{data?.map((product: any, key: number) => (
+				{filteredProducts?.map((product: any, key: number) => (
 					<div
 						key={key}
-						onClick={() =>
-							getObject(`/products/products/${productId}`, "GET", {})
-						}
+						onClick={() => {
+							getProduct(product?._id);
+							navigate(`/product/${product?._id}`);
+						}}
 						className="">
 						<ShopProducts
 							image={
