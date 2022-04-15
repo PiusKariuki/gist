d98f21b888b57d4254e315558d156831c38ede76
Fredrick Mundia <66358227+freddysundowner@users.noreply.github.com>
2022-04-15 20:58:31 +0300

    change events root

--- src/modules/Home/components/Upcoming/Upcoming.tsx
+++ src/modules/Home/components/Upcoming/Upcoming.tsx
@@ -60,34 +60,36 @@ const Upcoming = () => {
                 className=" bg-[rgba(0,0,0,.3)]  hover:bg-[rgba(0,0,0,.6)]   w-[3.125rem]
                         h-[3.125rem] rounded-full
                       z-40 hidden lg:flex lg:absolute  left-10 top-[50%]
-                        ">
-								<FontAwesomeIcon
-									onClick={() => scrollLeft(scrollRef)}
-									icon={faArrowLeft}
-									className="flex text-[1.25rem] self-center mx-auto text-[#00bcd7]"
-								/>
-							</div>
+                        "
+              >
+                <FontAwesomeIcon
+                  onClick={() => scrollLeft(scrollRef)}
+                  icon={faArrowLeft}
+                  className="flex text-[1.25rem] self-center mx-auto text-[#00bcd7]"
+                />
+              </div>
 
-							<div
-								className="bg-[rgba(0,0,0,.3)]  hover:bg-[rgba(0,0,0,.6)]  z-40 hidden
+              <div
+                className="bg-[rgba(0,0,0,.3)]  hover:bg-[rgba(0,0,0,.6)]  z-40 hidden
                         lg:flex lg:absolute text-[2rem]
-                        w-[3.125rem] h-[3.125rem] rounded-full right-10 top-[50%]">
-								<FontAwesomeIcon
-									onClick={() => scrollRight(scrollRef)}
-									icon={faArrowRight}
-									className="flex text-[1.25rem] self-center mx-auto text-[#00bcd7] hey"
-								/>
-							</div>
-						</>
-					) : null}
-				</>
-			) : (
-				<p className="text-black-40 text-[1rem] md:text-[1.6rem] font-[500] mb-[2rem]">
-					No rooms available.
-				</p>
-			)}
-		</div>
-	);
+                        w-[3.125rem] h-[3.125rem] rounded-full right-10 top-[50%]"
+              >
+                <FontAwesomeIcon
+                  onClick={() => scrollRight(scrollRef)}
+                  icon={faArrowRight}
+                  className="flex text-[1.25rem] self-center mx-auto text-[#00bcd7] hey"
+                />
+              </div>
+            </>
+          ) : null}
+        </>
+      ) : (
+        <p className="text-black-40 text-[1rem] md:text-[1.6rem] font-[500] mb-[2rem]">
+          No rooms available.
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Upcoming;
